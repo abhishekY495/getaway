0cbc4b2d2526faf37919b6166d08c1e3dd593821
Abhishek <abhishekY495.sec@gmail.com>
2026-08-07 22:02:34 +0530

    schema update

---
+++ apps/api/src/db/migrations/0004_dizzy_beast.sql
@@ -0,0 +1,4 @@
+ALTER TABLE "venues" ALTER COLUMN "description" SET NOT NULL;--> statement-breakpoint
+ALTER TABLE "venues" ALTER COLUMN "cover_image" SET NOT NULL;--> statement-breakpoint
+ALTER TABLE "venues" ALTER COLUMN "rating" SET NOT NULL;--> statement-breakpoint
+ALTER TABLE "venues" ALTER COLUMN "review_count" SET NOT NULL;
--- apps/api/src/db/schema.ts
+++ apps/api/src/db/schema.ts
@@ -80,13 +80,15 @@ export const venuesTable = pgTable("venues", {
     .references(() => citiesTable.id)
     .notNull(),
   name: text("name").notNull(),
-  description: text("description"),
-  coverImage: text("cover_image"),
+  description: text("description").notNull(),
+  coverImage: text("cover_image").notNull(),
   rating: numeric("rating", {
     precision: 2,
     scale: 1,
-  }).default("0"),
-  reviewCount: integer("review_count").default(0),
+  })
+    .default("0")
+    .notNull(),
+  reviewCount: integer("review_count").default(0).notNull(),
   createdAt: timestamp("created_at", { withTimezone: true })
     .defaultNow()
     .notNull(),
